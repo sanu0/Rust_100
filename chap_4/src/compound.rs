183ad909a4c3ddfff4b89a44ff1d5850c4342286
sanu0 <kumarsanu3703@gmail.com>
2026-03-16 16:04:08 +0530

    tuple data structure addition

---
+++ chap_4/src/compound.rs
@@ -0,0 +1,18 @@
+pub fn tupli(){
+    println!("Tuple data type....");
+    let tup1 : (i32, f64, u8) = (500, 6.4, 1);
+
+    println!("First value: {}", tup1.0);
+    println!("Second value: {}", tup1.1);
+    println!("Third value: {}", tup1.2);
+
+    let tup2 = (500, 6.4, 1);
+
+    let (x, y, z) = tup2;
+
+    println!("The value of y is: {y}");
+
+    let five_hundred = tup1.0;
+    println!("First element is : {}",five_hundred);
+    
+}
